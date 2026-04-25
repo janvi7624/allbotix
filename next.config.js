6867974cdd251e17d058953669ba5ee3fc0357e6
janvi7624 <janvi@nantatech.com>
2026-04-25 16:01:46 +0530

    Add CORS headers and rewrites to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 const nextConfig = {
-  output: "standalone",  
+  output: "standalone",
   reactStrictMode: true,
   images: {
-    unoptimized: true, 
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,PATCH,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
   },
 };
 
