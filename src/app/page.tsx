0a5d41a651c2a3a06da38940f7a448f7a8912a7a
Janvi Thummar <janvi@nantatech.com>
2026-04-25 13:01:16 +0530

    remove extra parts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Navbar                from '@/components/Navbar'
 import HeroSection       from '@/components/HeroSection'
 import TechSection       from '@/components/TechSection'
-import ProcessSection        from '@/components/ProcessSection'
-import StatsSection          from '@/components/extra/StatsSection'
 import SolutionsCarousel       from '@/components/SolutionsCarousel'
 import RobotShowcaseSection  from '@/components/RobotShowcaseSection'
-import PricingSection        from '@/components/extra/PricingSection'
 import FAQSection            from '@/components/FAQSection'
-import NewsletterSection     from '@/components/extra/NewsletterSection'
 import CtaBanner     from '@/components/CtaBanner'
 import Footer                from '@/components/Footer'
 
@@ -19,12 +15,8 @@ export default function Home() {
         <HeroSection />
         <TechSection />
         <RobotShowcaseSection />
-        {/* <ProcessSection /> */}
-        {/* <StatsSection /> */}
         <SolutionsCarousel />
-        {/* <PricingSection /> */}
         <FAQSection />
-        {/* <NewsletterSection /> */}
         <CtaBanner />
       </main>
       <Footer />
